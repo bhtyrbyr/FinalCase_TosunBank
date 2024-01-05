b970eaf40ca44346151b2bd417bc1171941a5653
bhtyrbyr <bhtyr.byr@gmail.com>
2024-01-05 14:48:30 +0300

    BaseEntity classes were created.

---
+++ Src/WebApi/FinalCase_TosunBank.WebApi/Controllers/StartController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FinalCase_TosunBank.WebApi.Controllers;
+
+[Route("api/v1/[controller]s")]
+[ApiController]
+public class StartController : ControllerBase
+{
+    public StartController()
+    {
+        
+    }
+
+    [HttpGet("")]
+    public async Task<IActionResult> GetAll()
+    {
+        return Ok("Selam Dünyalı");
+    }
+}
