d8f19f99f4ebac53805a586781d0c96890d4f942
bhtyrbyr <bhtyr.byr@gmail.com>
2024-01-06 19:58:03 +0300

    Tables have been added to the database where the bank system can operate at the basic level. Login system, authorisation system and token system were tested with new tables.

--- Src/Core/FinalCase_TosunBank.Application/DTOs/PersonRegisterDTO.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace FinalCase_TosunBank.Application.DTOs;
-
-public class PersonRegisterDTO
-{
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
-    public string Password { get; set; }
-    public string email { get; set; }
-}
